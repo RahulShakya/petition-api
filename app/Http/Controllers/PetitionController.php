e6986a91f6ec44fd338575660775885c4958cf03
Kumar <BAM20275@superioruniform.com>
2021-10-06 12:59:04 +0530

    petition get api done

--- app/Http/Controllers/PetitionController.php
+++ app/Http/Controllers/PetitionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PetitionResource;
 use App\Models\Petition;
 use Illuminate\Http\Request;
 
@@ -14,7 +15,8 @@ class PetitionController extends Controller
      */
     public function index()
     {
-        //
+        $petitions = Petition::all();
+        return PetitionResource::Collection(Petition::all());
     }
 
     /**
