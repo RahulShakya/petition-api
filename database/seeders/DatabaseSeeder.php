9e1261c1dbf23a56ad90a7508c5fe7a4a2897127
Kumar <BAM20275@superioruniform.com>
2021-10-06 10:23:06 +0530

    Laravel API

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use DB;
+use Str;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        App\Models\Petition::factory(10)->create();
+        // DB::table('petitions')->insert([
+        // 	'title'=>Str::random(10),
+        // 	'category'=>Str::random(10),
+        // 	'description'=>Str::random(100),
+        // 	'author'=>Str::random(100),
+        // ]);
+    }
+}
